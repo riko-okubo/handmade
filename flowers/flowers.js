d7024b6487ea371c398bed5aa1db0f8b230be374
riko-okubo <12riko14@gmailcom>
2021-04-12 11:49:13 +0900

    jsでの表示と画像追加

--- flowers/flowers.js
+++ flowers/flowers.js
@@ -7,7 +7,8 @@ var imglist = [
     {img: 'Tulip-red.jpg', alt: 'Tulip(red)'},
     {img: 'Tulip-blue.jpg', alt: 'Tulip(blue)'},
     {img: 'Tulip-yellow.jpg', alt: 'Tulip(yellow)'},
-    {img: 'Carnation.jpg', alt: 'Carnation'}
+    {img: 'Carnation.jpg', alt: 'Carnation'},
+    {img: 'camellia-pink1.jpg', alt: 'Camellia(pink)'}
 ];
 
 var src = imglist.length;
@@ -15,10 +16,10 @@ var src = imglist.length;
 document.write("<div class='contents'>");
 for (var i = 0; i < src; i++){
     document.write("<ul class='contents-item'>");
-    document.write("  <li>")
-    document.write("    <img src='" + imglist[i].img +"' >");
-    document.write("    <p>" + imglist[i].alt +"</p>");
-    document.write("  </li>");
+    document.write("<li>")
+    document.write("<img src='" + imglist[i].img +"' >");
+    document.write("<p>" + imglist[i].alt +"</p>");
+    document.write("</li>");
     document.write("</ul>");
 }
 document.write("</div>");
