0ca50e75dc81864e6bac89d9ef0e796448e2d09a
riko-okubo <12riko14@gmailcom>
2021-05-07 22:50:23 +0900

    同じ画像

--- earrings/stud.js
+++ earrings/stud.js
@@ -12,20 +12,45 @@ var imglist = [
     {img: 'img/red-tulip-earring1.jpg', alt: 'red tulip'},
 ] ;
 
+var modalimg = [
+    {img: 'img/stud-b.jpg'},
+    {img: 'img/stud-g.jpg'},
+    {img: 'img/stud-o.jpg'},
+    {img: 'img/stud-sb&w-2.jpg'},
+    {img: 'img/stud-r&w-2.jpg'},
+    {img: 'img/stud-v&p&w-2.jpg'},
+    {img: 'img/stud-o&g&y-2.jpg'},
+    {img: 'img/red-rose-earrings2.jpg'},
+    {img: 'img/red-rose-earring(s)1.jpg'},
+    {img: 'img/violet-rose-earring(s)1.jpg'},
+    {img: 'img/red-tulip-earring2.jpg'},
+];
+
 var src = imglist.length;
 
 document.write("<div class='contents'>");
-document.write("<ul class='contents-item'>");
+document.write("<ul class='contents-item' id='open'>");
 for (var i = 0; i < src; i++){
-    document.write("<li>")
+    document.write("<a id = 'modalOpen' class = 'modalOpen'>")
     document.write("<img src='" + imglist[i].img +"' >");
     document.write("<p>" + imglist[i].alt +"</p>");
-    document.write("</li>");
+    document.write("</a>");
 }
 document.write("</ul>");
 document.write("</div>");
 
-document.getElementById("modalOpen").addEventListener("click", function() {
+// document.write("<ul class='contents-item' id='mo'>");
+for (var i = 0; i < src; i++){
+    document.write("<div class = 'modal' id = 'modal'>")
+    document.write("<img src='" + modalimg[i].img +"' >");
+    document.write("<a id = 'modalClose' class = 'modalClose'>");
+    document.write("close");
+    document.write("</a>");
+    document.write("</div>");
+}
+// document.write("</ul>");
+
+document.getElementById("open").addEventListener("click", function() {
     document.getElementById("modal").classList.add("acutive");
     document.getElementById("mask").classList.add("acutive");
 })
